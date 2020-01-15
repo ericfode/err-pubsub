bc850066a9b64a38bc35767e4817a97921d9e5ac
Eric Fode <ericfode@circleci.com>
2020-01-15 14:16:36 -0800

    ugg hackery

--- pubsub.py
+++ pubsub.py
@@ -12,16 +12,14 @@ log = logging.getLogger(__name__)
 from typing import Callable
 
 
-def _tag_subhook(func, project, topic, sub):
-    log.info(f"webhooks:  Flag to bind {topic} to {getattr(func, '__name__', func)}")
-    func._err_pubsub_topic = topic
+def _tag_subhook(func, project, sub):
+    log.info(f"webhooks:  Flag to bind {sub} to {getattr(func, '__name__', func)}")
     func._err_pubsub_sub = project
     func._err_pubsub_project = sub
     return func
 
 
-def subhook(topic: str,
-            project: str ,
+def subhook(project: str,
             sub: str):
     """
     Decorator for webhooks
@@ -56,7 +54,7 @@ def subhook(topic: str,
             pass
     """
     def wrapped_sub(func):
-        return  _tag_subhook(func, project, topic, sub)
+        return  _tag_subhook(func, project, sub)
     return wrapped_sub
 
 APubSub = te.TypedDict('APubSub', {'PROJECT': str, 'SUBSCRIPTION': str, 'TOPIC':str})
@@ -64,10 +62,7 @@ APubSub = te.TypedDict('APubSub', {'PROJECT': str, 'SUBSCRIPTION': str, 'TOPIC':
 PubSubConfig = te.TypedDict('PubSubConfig', {'SERVICE_ACCOUNT_JSON': typing.Optional[str] }) 
 
 class Sub():
-    def __init__(self, project, topic, sub, callback: typing.Callable[[str], None]):
-        self.topic_name: str ='projects/{project_id}/topics/{topic}'.format(
-            project_id=project,
-            topic=topic) 
+    def __init__(self, project, sub, callback: typing.Callable[[str], None]):
         self.subscription_name: str='projects/{project_id}/subscriptions/{sub}'.format(
             project_id=project,
             sub=sub)
@@ -75,10 +70,8 @@ class Sub():
         self.callback: typing.Callable[[str], None] = callback
 
     def activate(self, subscriber):
-        subscriber.create_subscription(
-            name= self.subscription_name,
-            topic= self.topic_name
-        )
+        print("creating sub")
+        print("activating sub")
         subscriber.subscribe(self.subscription_name,
                             self.callback)
 
@@ -104,16 +97,14 @@ class PubSub(BotPlugin):
        return {'SERVICE_ACCOUNT_JSON': None }
 
     def check_configuration(self, configuration):
-        print("checking config")
         if not isinstance(configuration , PubSubConfig):
             raise errbot.ValidationException('PubSubConfig is broken')
         super().check_configuration(configuration)
 
     def configure(self, configuration: typing.Mapping) -> None:
-        print("CONFIGURE")
-        print("CONFIG: ", configuration)
         self.config = configuration
-        if 'SERVICE_ACCOUNT_JSON' in self.config:
+        print("CONFIG")
+        if self.config is not None and 'SERVICE_ACCOUNT_JSON' in self.config:
             self.service_account_info = json.load(open(self.config['SERVICE_ACCOUNT_JSON']))
         self.audience = "https://pubsub.googleapis.com/google.pubsub.v1.Subscriber"
 
@@ -126,35 +117,33 @@ class PubSub(BotPlugin):
     def find_subs(self, obj):
         """Checks a plugin for sns listeners and attaches callbacks if they are needed"""
         classname = obj.__class__.__name__
-        self.log.info("Checking %s for pubsub hooks", classname)
+        print("Checking %s for pubsub hooks", classname)
         for name, func in getmembers(obj, ismethod):
             if getattr(func, '_err_pubsub_topic', False): # False is the default value
-                self.log.info("pubsub routing %s, from %s.%s",
+                print("pubsub routing %s, from %s.%s",
                               func.__name__,
-                              func._err_pubsub_topic,
                               func._err_pubsub_sub)
                 new_sub = Sub(
-                    func._err_pubsub_topic,
                     func._err_pubsub_sub,
                     func._err_pubsub_project,
                     func)
                 self.subs.append(new_sub)
 
     def activate(self):
-        self.log.warn('Starting PubSubListener')
+        self.log.info('Starting PubSubListener')
+        self.log.info('Where is my log')
         self.subscriber = pubsub_v1.SubscriberClient()
         super().activate()
         pm = self._bot.plugin_manager
         plugs = pm.get_all_active_plugins()
-        print("TEST")
-        print(plugs)
-        for p in plugs:
-            self.find_subs(p)
+        if plugs:
+            for p in plugs:
+                self.find_subs(p)
 
-        if self.sub is not None:
+        print("subs" , self.subs)
+        if self.subs is not None:
             for sub in self.subs:
-                self.log.info('listening to topic: {topic} and sub: {sub}'.format(
-                    topic=sub.topic_name, sub=sub.subscription_name))
+                self.log.info('listening to sub: {sub}'.format(sub=sub.subscription_name))
                 try:
                     sub.activate(self.subscriber)
                 except Exception:
